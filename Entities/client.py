d5b6e7e7b7bba50007786734951c8f97f70153e4
NunzioDA <nunziodamore99@gmail.com>
2024-04-24 11:42:31 +0200

    First files commit

---
+++ Entities/client.py
@@ -0,0 +1,10 @@
+class Client:
+      
+    def __init__(self, address, name):
+        self.address = address
+        self.name = name
+
+    def send_message(text):
+        raise NotImplementedError("Not implemented")
+    
+    
---
+++ Entities/server.py
@@ -0,0 +1,11 @@
+class Client:
+      
+    def __init__(self, address, name):
+        self.address = address
+        self.name = name
+
+    def duplicate_data(text):
+        raise NotImplementedError("Not implemented")
+    
+    def start_messages_listener(port):
+        raise NotImplementedError("Not implemented")
