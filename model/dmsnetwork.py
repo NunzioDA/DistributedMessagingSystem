dce30de291387a2a712689e1b9c6c22d60e3287b
NunzioDA <nunziodamore99@gmail.com>
2024-05-24 14:55:32 +0200

    Spostata tutta la gestione del file servers.json all'interno di dmsnetwork con gestione di lock per l'accesso concorrente

--- model/dmsnetwork.py
+++ model/dmsnetwork.py
@@ -2,6 +2,7 @@ import json
 import os
 import threading
 import communication_API.elite_servers_API as elite_server
+from dms_secure.hashing import hash_to_range
 
 
 class DMSNetwork:    
@@ -13,6 +14,7 @@ class DMSNetwork:
         self.NETWOWK_PATH = base_data_path + "/network/"
         self.NETWOWK_FILE_PATH = self.NETWOWK_PATH + "/servers.json"
         self.discovering_thread_active = False
+        self.network_file_lock = threading.Lock()
         self.init_path()
 
     def init_path(self):
@@ -50,8 +52,7 @@ class DMSNetwork:
                     if(not there_are_empty_clusters):
                         self.stop_discoverer_thread()
 
-                    with open(self.NETWOWK_FILE_PATH, "w") as servers_file:
-                        json.dump(result, servers_file)
+                    self.save_network(result)
                     break
             
             if(result!=False):
@@ -63,6 +64,53 @@ class DMSNetwork:
 
             return result
     
+    def save_network(self, content):
+        self.network_file_lock.acquire()
+        with open(self.NETWOWK_FILE_PATH, "w") as servers_file:
+            json.dump(content, servers_file)
+        self.network_file_lock.release()
+
+    def get_network(self):
+        self.network_file_lock.acquire()
+        with open(self.NETWOWK_FILE_PATH, "r") as servers_file:
+            network = json.load(servers_file)
+        self.network_file_lock.release()
+
+        return network    
+
+    def clusters_number(self):
+        return len(self.get_network())
+
+    def my_cluster_id(self):
+        if(self.is_server):
+            tag = str(self.address)
+        else:
+            tag = self.username
+
+        return hash_to_range(tag.encode(), self.clusters_number())
+
+    def my_cluster(self):
+
+        if(self.is_server):
+            tag = str(self.address)
+        else:
+            tag = self.username
+
+        clusters = self.get_network()        
+        my_cluster_id = hash_to_range(tag.encode(), len(clusters))
+                
+        my_cluster = clusters[my_cluster_id]
+        if(self.is_server):
+            my_cluster.remove(self.address)
+        return my_cluster
+
+    def add_server_to_network(self, server_address):
+        new_server_cluster = hash_to_range(str(server_address).encode(), self.clusters_number())
+        clusters = self.get_network()
+        if(server_address not in clusters[new_server_cluster]):
+            clusters[new_server_cluster].append(server_address)
+            self.save_network(clusters)
+
     def _dicover_thread(self):
         while self.discovering_thread_active:
             self.discover()
